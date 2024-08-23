dca3eb7cf847471572380664df29ac49e2e6416c
Arbab Memon <139270412+IamArbabMemon@users.noreply.github.com>
2024-08-24 03:22:11 +0500

    Update passport.google.ts

--- src/passport.google.ts
+++ src/passport.google.ts
@@ -2,8 +2,8 @@ const googlePassport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 googlePassport.use(new GoogleStrategy({
-    clientID: '22182420256-bcvlntc1s77g9ohul3p3ef4sghk51bms.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientID: '',
+    clientSecret: '',
     callbackURL: "http://localhost:3000/auth/google/callback"
   },
 
